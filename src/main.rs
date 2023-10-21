0b4d42c90233e6addcad9cf98249f352ca2b9c40
Nikolaj Jehøj-Krogager <kagenogmig@gmail.com>
2023-10-21 15:22:29 +0200

    Add stuff :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use sqlite;
+use std::fs::remove_file;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Opening connection to database... ");
+    let connection = sqlite::open(get_database_name()).unwrap();
+    println!("done.");
+    delete_database();
+    println!("See you next time :)");
+}
+
+fn delete_database() {
+    match remove_file(get_database_name()) {
+        Ok(_) => println!("Succesfully deleted database."),
+        Err(_) => println!("An error happened while deleting database.")
+    };
+}
+
+fn get_database_name() -> String {
+    String::from("db.db")
 }
